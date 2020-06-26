9b3213e617b3e3242c9599c6a88a0dc8f2846f2f
Michał Mytnik <Michal.Mytnik@kruksa.pl>
2020-06-26 13:40:50 +0200

    Added new column/variable

--- Quiz/Answer.cs
+++ Quiz/Answer.cs
@@ -11,6 +11,7 @@ namespace Quiz
         int AnswerId {get; set;}
         int QuestionId { get; set; }
         string AnswerTheQuestion { get; set; }
+        private bool CorrectAnswer { get; set; }
         public virtual Question Question { get; set; }
     }
 }
